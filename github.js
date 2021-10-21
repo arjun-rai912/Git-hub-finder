3547da6e905e78d0e698b44916b8a454065d52b3
Arjun Rai <arjunarjun2222@gmail.com>
2021-10-22 02:59:00 +0530

    yehkoitohpadhlo

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '33bc10e2c26704fecb00';
+    this.client_secret = 'f4dbb45b1d19ee05eb1feef962241a7346ec9344';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
